02331e1d73f9c222155851e72a354d626bdfaf0a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2022-12-13 19:25:17 +0400

    animation website

--- js102/src/script/script.js
+++ js102/src/script/script.js
@@ -4,4 +4,75 @@ for (var i = 0; i < people.length; i++) {
     console.log(people[i])
 }
 
-console.log("people")
+// for(indexi in masivi) { 
+// moqmedebebi
+// }
+
+
+var people = ["Tom", "Alice", "Bob", "Sam"];
+
+// for(var index in people) {
+//   console.log(people[index])
+// }
+
+var people = ["Tom", "Alice", "Bob", "Sam"];
+
+var index = 0;
+
+while (index < people.length) {
+    console.log(people[index])
+    index++;
+}
+
+function display() {
+    document.write("Hello World")
+
+
+}
+
+function goodMorning() {
+    document.write("good morning")
+}
+
+function goodMorning() {
+    document.write("good night")
+}
+
+var message = goodEvening
+message();
+
+///////////////
+
+function display(x, y) {
+    if (y === undefined) y = x;
+    var z = x * x;
+    console.log(2)
+
+
+}
+
+
+
+function display() {
+    var z = 1;
+    for (var i = 0; i < arguments.length; i++)
+        z = arguments[i]
+    console.log(z)
+}
+
+// display[6]
+// display(6,4,5)
+
+function display(x, func) {
+    var message = func(x);
+    document.write(message)
+}
+
+function welcomeMessage(time) {
+    if (time < 12)
+        return "good morning"
+    else
+        return "good evening"
+}
+
+display(13, welcomeMessage)
