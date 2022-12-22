458c65a376aa725b1b8142fe6a6e51506b4ada25
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2022-12-22 22:20:06 +0400

    final project final update

---
+++ Final Project/Ver2/script.js
@@ -0,0 +1,8 @@
+/* ========================= typing animation ================== */
+
+var typed = new Typed(".typing", {
+    strings: ["Frontend Developer", "Java Developer", "Web Developer", "Javascript Developer", "C++ Developer", "Markup Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
