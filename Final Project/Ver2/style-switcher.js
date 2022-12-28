f8b6f3b2ccdf6069549fbdd44757d6569d16b249
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2022-12-28 21:53:32 +0400

    upd

--- Final Project/Ver2/style-switcher.js
+++ Final Project/Ver2/style-switcher.js
@@ -2,7 +2,7 @@
 
 
 
-const daNight = document.querySelector(".day-night");
+const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
